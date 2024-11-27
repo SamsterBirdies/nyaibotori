3eca03d9837cd0a46073b0c0ca948c7539440540
SamsterBirdies <71206056+SamsterBirdies@users.noreply.github.com>
2024-11-26 20:50:15 -0800

    Update character list

--- commands/utility/genshin_character.js
+++ commands/utility/genshin_character.js
@@ -14,21 +14,27 @@ const characters =
 [
 	'Albedo','Alhaitham','Aloy','Amber','Arataki Itto','Arlecchino',
 	'Baizhu','Barbara','Beidou','Bennett',
-	'Candace','Charlotte','Chevreuse','Chiori','Chongyun','Clorinde','Collei','Cyno',
-	'Dehya','Diluc','Diona','Dori',
+	'Candace','Charlotte','Chevreuse','Chiori','Chongyun','Clorinde','Collei','Cyno','Chasca','Citlali',
+	'Dehya','Diluc','Diona','Dori','Dainsleif',
 	'Emilie','Eula',
 	'Faruzan','Fischl','Freminet','Furina',
 	'Gaming','Ganyu','Gorou',
 	'Hu Tao',
 	'Jean',
 	'Kachina','Kaedehara Kazuha','Kaeya','Kamisato Ayaka','Kamisato Ayato','Kaveh','Keqing','Kinich','Kirara','Klee','Kujou Sara','Kuki Shinobu',
-	'Layla','Lisa','Lynette','Lyney',
-	'Mika','Mona','Mualani',
+	'Layla','Lisa','Lynette','Lyney','Lan Yan',
+	'Mika','Mona','Mualani','Mavuika',
 	'Nahida','Navia','Neuvillette','Nilou','Ningguang','Noelle',
+	'Ororon',
 	'Qiqi',
 	'Raiden Shogun','Razor','Rosaria',
 	'Sangonomiya Kokomi','Sayu','Sethos','Shenhe','Shikanoin Heizou','Sigewinne','Sucrose',
-	'Tartaglia','Thoma','Tighnari'
+	'Tartaglia','Thoma','Tighnari',
+	'Venti',
+	'Wanderer','Wriothesley',
+	'Xiangling','Xianyun','Xiao','Xilonen','Xingqiu','Xinyan',
+	'Yae Miko','Yanfei','Yaoyao','Yelan','Yoimiya','Yun Jin',
+	'Zhongli,'
 ];
 module.exports = {
 	data: command,
--- commands/utility/random_GI_character.js
+++ commands/utility/random_GI_character.js
@@ -11,25 +11,31 @@ module.exports = {
 	data: command,
 	async execute(interaction) {
 		const visible = interaction.options.getBoolean('ephemeral') ?? 'false';
-		const commanders =
+		const characters =
 		[
 			'Albedo','Alhaitham','Aloy','Amber','Arataki Itto','Arlecchino',
 			'Baizhu','Barbara','Beidou','Bennett',
-			'Candace','Charlotte','Chevreuse','Chiori','Chongyun','Clorinde','Collei','Cyno',
-			'Dehya','Diluc','Diona','Dori',
+			'Candace','Charlotte','Chevreuse','Chiori','Chongyun','Clorinde','Collei','Cyno','Chasca','Citlali',
+			'Dehya','Diluc','Diona','Dori','Dainsleif',
 			'Emilie','Eula',
 			'Faruzan','Fischl','Freminet','Furina',
 			'Gaming','Ganyu','Gorou',
 			'Hu Tao',
 			'Jean',
 			'Kachina','Kaedehara Kazuha','Kaeya','Kamisato Ayaka','Kamisato Ayato','Kaveh','Keqing','Kinich','Kirara','Klee','Kujou Sara','Kuki Shinobu',
-			'Layla','Lisa','Lynette','Lyney',
-			'Mika','Mona','Mualani',
+			'Layla','Lisa','Lynette','Lyney','Lan Yan',
+			'Mika','Mona','Mualani','Mavuika',
 			'Nahida','Navia','Neuvillette','Nilou','Ningguang','Noelle',
+			'Ororon',
 			'Qiqi',
 			'Raiden Shogun','Razor','Rosaria',
 			'Sangonomiya Kokomi','Sayu','Sethos','Shenhe','Shikanoin Heizou','Sigewinne','Sucrose',
-			'Tartaglia','Thoma','Tighnari'
+			'Tartaglia','Thoma','Tighnari',
+			'Venti',
+			'Wanderer','Wriothesley',
+			'Xiangling','Xianyun','Xiao','Xilonen','Xingqiu','Xinyan',
+			'Yae Miko','Yanfei','Yaoyao','Yelan','Yoimiya','Yun Jin',
+			'Zhongli,'
 		];
 		var selected_character = commanders[Math.floor(Math.random() * commanders.length)]
 		await interaction.reply({ content: 'https://genshin-impact.fandom.com/wiki/' + selected_character.replace(" ", "_"), ephemeral: visible });
